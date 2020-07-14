a7e9c083acbbf6c99032d0fdc238022eb1b5e691
Jerome Aragones <joni.jinon@awsys-i.com>
2020-07-14 18:43:29 +0800

    ReactJS_Training_D07_ScreamDialog

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,4 +1,4 @@
-import { SET_SCREAMS, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_DATA, DELETE_SCREAM, POST_SCREAM } from '../types';
+import { SET_SCREAMS, SET_SCREAM, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_DATA, DELETE_SCREAM, POST_SCREAM } from '../types';
 
 const initialState = {
     screams: [],
@@ -12,13 +12,18 @@ export default function (state = initialState, action){
             return {
                 ...state,
                 loading:true
-            }
+            };
         case SET_SCREAMS:
             return {
                 ...state,
                 screams: action.payload,
                 loading: false
-            }
+            };
+        case SET_SCREAM:
+            return {
+                ...state,
+                scream: action.payload
+            };
         case LIKE_SCREAM:
         case UNLIKE_SCREAM:
             var index = state.screams.findIndex((scream) => scream.screamId === action.payload.screamId);
