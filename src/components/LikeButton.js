e9c41fb1d6dea66c95bf86c524ddd622c40ec62f
Jerome Aragones <joni.jinon@awsys-i.com>
2020-07-14 20:24:14 +0800

    ReactJS_Training_D07_ScreamDialogDetails_FixedErrors

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,6 +1,6 @@
  import React, { Component } from 'react';
  import MyButton from '../util/MyButton';
- import { Link } from 'react-dom';
+ import { Link } from 'react-router-dom';
  import PropTypes from 'prop-types';
 
  // Icons
